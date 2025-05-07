dbcc1cbafc2bc3d991a878345cafb48aac874dfa
praveendevopsk8s <149125849+praveendevopsk8s@users.noreply.github.com>
2025-05-07 23:03:59 +0530

    Update jenkinsscript5.sh

--- jenkinsscript5.sh
+++ jenkinsscript5.sh
@@ -3,8 +3,8 @@
 set -e  # Exit on any error
 
 echo "### Updating and installing system dependencies ###"
-sudo apt-get update
-sudo apt-get install -y python3 python3-venv python3-pip curl
+su apt-get update
+su apt-get install -y python3 python3-venv python3-pip curl
 
 echo "### Python version check ###"
 python3 -V
